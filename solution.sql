7ffbbf47b436bd379eb7a25b3be8cdfdbca9ac1e
GINI TYSON <ginityson@yahoo.com>
2016-06-14 20:15:42 -0500

    corrected my copy and paste work

--- solution.sql
+++ solution.sql
@@ -10,21 +10,21 @@ WHERE username LIKE '%a%';
 --   with an account balance of 0.00 and a transactions_attempted of 0.
 --   Give them a new account balance of 10.00.
 UPDATE syntax_practice
-set account_balance='10.00'
-where account_balance='0.00'
+SET account_balance='10.00'
+WHERE account_balance='0.00'
 AND transactions_attempted='0';
 
 
 -- 4 Select all users that have attempted 9 or more transactions.
+SELECT * FROM syntax_practice
+WHERE transactions_attempted>'9';
+
+-- 5 Get the username and account balance of the 3 users with the highest balances,
+--sort highest to lowest balance. NOTE: Research LIMIT
 SELECT username, account_balance
 FROM syntax_practice
 ORDER BY account_balance DESC
 LIMIT 3;
-
--- 5 Get the username and account balance of the 3 users with the highest balances,
---sort highest to lowest balance. NOTE: Research LIMIT
-SELECT * FROM syntax_practice
-ORDER BY account_balance DESC;
 
 -- 6 Get the username and account balance of the 3 users with the lowest balances,
 --    sort lowest to highest balance.
@@ -48,4 +48,4 @@ INSERT INTO syntax_practice (username, city, transactions_completed, transaction
 --    and have completed fewer than 5 transactions.
 DELETE FROM syntax_practice
 WHERE city='miami' or city='phoenix'
-AND transactions_attempted<5;   
+AND transactions_attempted<5;
